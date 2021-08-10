7c797fc6d2314225575f4701f0c05ede66d0dea4
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-08-10 20:33:14 +0530

    add santize middleware

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const xss = require('xss-clean');
+const mongoSanititize = require('express-mongo-sanitize');
 
-const port = process.env.PORT || 3000;
+// sanitize
+app.use(xss());
+app.use(mongoSanititize());
 
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
