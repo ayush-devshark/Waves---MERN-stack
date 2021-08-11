d0a0e2de2d193ad71b4087d634c33b2a9d086db6
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-08-11 18:08:04 +0530

    add routes, coontroller, services boilerplate

--- server/server.js
+++ server/server.js
@@ -3,10 +3,19 @@ const app = express();
 const mongoose = require('mongoose');
 const xss = require('xss-clean');
 const mongoSanititize = require('express-mongo-sanitize');
+const routes = require('./routes');
+
+// MIDDLEWARE
+
+// json
+app.use(express.json());
 
 // sanitize
 app.use(xss());
 app.use(mongoSanititize());
+
+// routes
+app.use(routes);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
