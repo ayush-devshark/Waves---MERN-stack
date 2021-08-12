b7e4f954f771f1b3e823d8174d9d21999b4c2b67
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-08-12 19:17:23 +0530

    connect to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const mongoose = require('mongoose');
 const xss = require('xss-clean');
 const mongoSanititize = require('express-mongo-sanitize');
 const routes = require('./routes');
 
-// MIDDLEWARE
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
 
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+// MIDDLEWARE
 // json parse
 app.use(express.json());
 
