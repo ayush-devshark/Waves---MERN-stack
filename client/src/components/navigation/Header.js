03f6ef433a2cd9add36a2cf57c61066938821b1a
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-08-29 22:41:01 +0530

    add Header and Footer

--- client/src/components/navigation/Header.js
+++ client/src/components/navigation/Header.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return <>Header</>;
+    return (
+        <header className='bck_b_light'>
+            <div className='container'>
+                <div className='left'>
+                    <div className='logo'>WAVES</div>
+                </div>
+                <div className='right'>
+                    <div className='top'>
+                        <>
+                            <div className='cart_link'>
+                                <span>0</span>
+                                <Link to='/dashboard/user/user_cart'>
+                                    My Cart
+                                </Link>
+                            </div>
+
+                            <Link to='/dashboard'>My Account</Link>
+                            <span
+                                onClick={() => {
+                                    alert('log out');
+                                }}
+                            >
+                                Logout
+                            </span>
+
+                            <Link to='/sign_in'>Login</Link>
+                        </>
+                    </div>
+                    <div className='bottom'>
+                        <Link to='/'>Home</Link>
+                        <Link to='/shop'>Shop</Link>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
