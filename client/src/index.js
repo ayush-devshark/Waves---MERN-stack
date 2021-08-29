9691611bd1c3a96565ab75ba4b5285c4ed7eaa02
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-08-29 21:30:57 +0530

    add routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes';
-import './resources/styles/styles.css';
+import App from 'routes';
+import 'resources/styles/styles.css';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,18 @@
-function Routes() {
-    return <div>Waves App</div>;
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import Header from 'components/navigation/Header';
+import Footer from 'components/navigation/Footer';
+import Home from 'components/home';
+
+function App() {
+    return (
+        <Router>
+            <Header />
+            <Switch>
+                <Route path='/' exact component={Home} />
+            </Switch>
+            <Footer />
+        </Router>
+    );
 }
 
-export default Routes;
+export default App;
