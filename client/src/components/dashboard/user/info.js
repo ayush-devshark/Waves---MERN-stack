7db042d712a62d84fce3de4a8f7a1a7041958d29
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-09-11 15:21:48 +0530

    add email stepper

--- client/src/components/dashboard/user/info.js
+++ client/src/components/dashboard/user/info.js
@@ -3,6 +3,7 @@ import DashboardLayout from 'hoc/DashboardLayout';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { errorHelper } from 'utils/tools';
+import EmailStepper from './stepper';
 import { useDispatch } from 'react-redux';
 import { userUpdateProfile } from 'store/actions/users.actions';
 
@@ -68,6 +69,10 @@ const UserInfo = ({ users }) => {
                     Edit profile
                 </Button>
             </form>
+            <hr />
+            <div>
+                <EmailStepper users={users} />
+            </div>
         </DashboardLayout>
     );
 };
