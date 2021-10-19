12135a552d074e8edab8b8d7726725635de2a64d
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-10-19 19:41:53 +0530

    add Product Carrousel

--- client/src/components/product/index.js
+++ client/src/components/product/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ProdInfo from './ProdInfo';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { productById } from 'store/actions/products.actions';
@@ -7,11 +8,33 @@ import { clearCurrentProduct } from 'store/actions/index';
 import Loader from 'utils/loader';
 import { renderCartImage } from 'utils/tools';
 
-import ProdInfo from './ProdInfo';
+import { Modal } from 'react-bootstrap';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const ProductDetail = props => {
+    const [modal, setModal] = useState(false);
     const products = useSelector(state => state.products);
     const dispatch = useDispatch();
+
+    const sliderSettings = {
+        dot: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
+    const handleModalClose = () => {
+        setModal(false);
+    };
+
+    const handleCarrousel = () => {
+        if (products.byId.images.length > 0) {
+            setModal(true);
+        }
+    };
 
     useEffect(() => {
         dispatch(productById(props.match.params.id));
@@ -36,7 +59,7 @@ const ProductDetail = props => {
                                 <img
                                     src={renderCartImage(products.byId.images)}
                                     alt='some'
-                                    onClick={() => null}
+                                    onClick={() => handleCarrousel()}
                                 />
                             </div>
                         </div>
@@ -48,6 +71,33 @@ const ProductDetail = props => {
                     <Loader />
                 )}
             </div>
+            <Modal
+                show={modal}
+                onHide={handleModalClose}
+                dialogClassName='modal-90w'
+            >
+                <Modal.Header closeButton>
+                    <Modal.Body>
+                        <Slider {...sliderSettings}>
+                            {products.byId && products.byId.images
+                                ? products.byId.images.map(item => (
+                                      <div
+                                          key={item}
+                                          style={{ margin: '0 auto' }}
+                                      >
+                                          <div
+                                              className='img-block'
+                                              style={{
+                                                  background: `url(${item}) no-repeat`,
+                                              }}
+                                          ></div>
+                                      </div>
+                                  ))
+                                : null}
+                        </Slider>
+                    </Modal.Body>
+                </Modal.Header>
+            </Modal>
         </div>
     );
 };
